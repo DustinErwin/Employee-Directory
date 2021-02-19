269a2c0bf8f572b8469568a3f4d96f15352ea64c
Galahad <derwin499@gmail.com>
2021-02-19 12:52:54 -0500

    created initial employee generation

--- src/components/TableBody/index.js
+++ src/components/TableBody/index.js
@@ -1,13 +1,24 @@
 import React from "react";
 import TableRow from "../TableRow";
+
 import "./style.css";
 
-function TableBody() {
-  return (
-    <tbody className="table-body">
-      <TableRow />
-    </tbody>
-  );
-}
+let tableRows = [];
+let initialize = false;
 
+const initialTable = () => {
+  for (let i = 0; i < 20; i++) {
+    tableRows.push(<TableRow />);
+  }
+  initialize = true;
+};
+
+if (!initialize) {
+  initialTable();
+}
+class TableBody extends React.Component {
+  render() {
+    return <tbody className="table-body">{tableRows}</tbody>;
+  }
+}
 export default TableBody;
--- src/components/TableRow/index.js
+++ src/components/TableRow/index.js
@@ -1,13 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./style.css";
 
 function TableRow() {
+  const [emp, setEmp] = useState({
+    name: "",
+    email: "",
+    years: "",
+    phone: "",
+    pic: "",
+  });
+
+  const fetchThis = () =>
+    fetch("https://randomuser.me/api/")
+      .then((response) => response.json())
+      .then((data) => {
+        setEmp({
+          ...emp,
+          name: `${data.results[0].name.first} ${data.results[0].name.last}`,
+          email: data.results[0].email,
+          years: data.results[0].registered.age,
+          phone: data.results[0].phone,
+          pic: data.results[0].picture.large,
+        });
+      });
+  useEffect(() => {
+    fetchThis();
+  }, []);
+
   return (
-    <tr className="table-row">
-      <td>Dustin Erwin</td>
-      <td>derwin499@gmail.com</td>
-      <td>(603)507-4475</td>
-      <td>8</td>
+    <tr className="table-row" key={emp.name}>
+      <td>{emp.name}</td>
+      <td>{emp.email}</td>
+      <td>{emp.phone}</td>
+      <td>{emp.years}</td>
     </tr>
   );
 }
